934a5daee3151a096ed4519a2cd580803f6f6c77
Philippe Chadenier <phil.chadenier@gmail.com>
2019-11-21 17:18:15 +0100

    Start handling architecture

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public enum status {
+        Locked = 1 << 1,
+        Available = 1 << 2,
+        Done = 1 << 3
+    };
+    public enum level {
+        ONE, TWO, THREE
+    };
+    private string levelName;
+    public MeshRenderer mesh;
+    public level difficulty;
+    public status levelStatus;
+    public Vector3  finalRotation;
+
+    void Start()
+    {
+
+    }
+
+    void CheckRotation() {
+        if ()
+    }
+    void Update()
+    {
+        CheckRotation();
+    }
+}
