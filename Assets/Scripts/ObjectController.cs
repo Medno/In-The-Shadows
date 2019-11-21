4bcd23c0b870b79be980a8c130c8975ac9551f05
Philippe Chadenier <phil.chadenier@gmail.com>
2019-11-21 15:56:06 +0100

    Bound movements

--- Assets/Scripts/ObjectController.cs
+++ Assets/Scripts/ObjectController.cs
@@ -7,30 +7,55 @@ public class ObjectController : MonoBehaviour
     public float horizontalSpeed = 1.0f;
     public float verticalSpeed = 1.0f;
     private Vector3 screenPoint;
-    void Start()
-    {
-
-    }
-
-    void    OnMouseDown()
+    void OnMouseDown()
     {
         screenPoint = new Vector3(0.0f, 0.0f, 0.0f);
     }
-    void    OnMouseDrag()
+    void ResetOffset()
     {
         if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
             screenPoint.x = 0.0f;
         else if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.RightControl))
             screenPoint.y = 0.0f;
 
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+            screenPoint.x = 0.0f;
+        else if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+            screenPoint.y = 0.0f;
+    }
+    void MoveObject()
+    {
+        Vector3 limitPosition = transform.position;
+        screenPoint.y += Input.GetAxis("Mouse Y");
+        float   translation = Time.deltaTime * Input.GetAxis("Mouse Y");
+        if (transform.position.y < 6.0f)
+        {
+            screenPoint.y = 0.0f;
+            limitPosition.y = 6.0f;
+        }
+        else if (transform.position.y > 12.0f)
+        {
+            screenPoint.y = 0.0f;
+            limitPosition.y = 12.0f;
+        }
+        else
+            limitPosition.y += screenPoint.y * Time.deltaTime;
+        transform.position = limitPosition;
+    }
+    void RotateObject()
+    {
         if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
             screenPoint.y += Input.GetAxis("Mouse Y");
         else
             screenPoint.x += Input.GetAxis("Mouse X");
         transform.Rotate(verticalSpeed * screenPoint.y * Time.deltaTime, horizontalSpeed * screenPoint.x * Time.deltaTime, 0);
     }
-    void Update()
+    void OnMouseDrag()
     {
-
+        ResetOffset();
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            MoveObject();
+        else
+            RotateObject();
     }
 }
